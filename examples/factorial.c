429d0cab37f473832bd01eb041536b1b2b46ff8e
Max Bernstein <tekknolagi@gmail.com>
2014-02-15 16:20:34 -0700

    easier instruction loading

--- examples/factorial.c
+++ examples/factorial.c
@@ -3,16 +3,16 @@
 int main (int argc, char **argv) {
   carp_instruction program[] = {
     // compute 5 factorial
-    carp_encode(CARP_INSTR_LOADI,  CARP_REG0,    1,    0), // loadi r0 1
-    carp_encode(CARP_INSTR_LOADI,  CARP_REG1,    5,    0), // loadi r1 5
-    carp_encode(CARP_INSTR_LOADI,  CARP_REG2,    1,    0), // loadi r2 0
-    carp_encode(CARP_INSTR_MUL  ,  CARP_REG0, CARP_REG1, CARP_REG0), // mul r0 r1 r0
-    carp_encode(CARP_INSTR_DECR ,  CARP_REG1,    0,    0), // decr r1
-    carp_encode(CARP_INSTR_CMP  ,  CARP_REG1, CARP_REG2,    0), // cmp r1 r2
-    carp_encode(CARP_INSTR_RJZ  ,     2,    0,    0), // rjz 2 (jump forward +2 if eax is 0)
-    carp_encode(CARP_INSTR_JMP  ,     3,    0,    0), // jmp to more multiplication
-    carp_encode(CARP_INSTR_SHOW ,  CARP_REG0,    0,    0), // show r0
-    carp_encode(CARP_INSTR_HALT ,     0,    0,    0), // halt 0
+    {CARP_INSTR_LOADI, {CARP_REG0, 1}}, // loadi r0 1
+    {CARP_INSTR_LOADI, {CARP_REG1, 5}}, // loadi r1 5
+    {CARP_INSTR_LOADI, {CARP_REG2, 1}}, // loadi r2 1
+    {CARP_INSTR_MUL, {CARP_REG0, CARP_REG1, CARP_REG0}}, // mul r0 r1 r0
+    {CARP_INSTR_DECR, {CARP_REG1}}, // decr r1
+    {CARP_INSTR_CMP, {CARP_REG1, CARP_REG2}}, // cmp r1 r2
+    {CARP_INSTR_RJZ, {2}}, // rjz 2 (jump forward +2 if eax is 0)
+    {CARP_INSTR_JMP, {3}}, // jmp to more multiplication
+    {CARP_INSTR_SHOW, {CARP_REG0}}, // show r0
+    {CARP_INSTR_HALT, {0}} // halt 0
   };
 
   carp_vm_do(program);
--- examples/sum_numbers.c
+++ examples/sum_numbers.c
@@ -3,17 +3,17 @@
 int main (int argc, char **argv) {
   carp_instruction program[] = {
     // sum numbers from 1 to 100
-    carp_encode(CARP_INSTR_LOADI,  CARP_REG0,     0,     0), // loadi r0 0
-    carp_encode(CARP_INSTR_LOADI,  CARP_REG1,   100,     0), // loadi r1 100
-    carp_encode(CARP_INSTR_LOADI,  CARP_REG2,     0,     0), // loadi r2 0
-    carp_encode(CARP_INSTR_ADD  ,  CARP_REG0,  CARP_REG1,  CARP_REG0), // add r0 r1 r0
-    carp_encode(CARP_INSTR_DECR ,  CARP_REG1,     0,     0), // decr r1
-    carp_encode(CARP_INSTR_CMP  ,  CARP_REG1,  CARP_REG2,     0), // cmp r0 r0
-    carp_encode(CARP_INSTR_RJZ  ,     2,     0,     0), // rjz 1 (jump forward +2 if eax is 0)
-                                              // that is, to the halt instruction
-    carp_encode(CARP_INSTR_JMP  ,     3,     0,     0), // jmp to more addition
-    carp_encode(CARP_INSTR_SHOW ,  CARP_REG0,     0,     0), // show r0
-    carp_encode(CARP_INSTR_HALT ,     0,     0,     0), // halt 0
+    {CARP_INSTR_LOADI, {CARP_REG0, 0}}, // loadi r0 0
+    {CARP_INSTR_LOADI, {CARP_REG1, 100}}, // loadi r1 100
+    {CARP_INSTR_LOADI, {CARP_REG2, 0}}, // loadi r2 0
+    {CARP_INSTR_ADD, {CARP_REG0, CARP_REG1, CARP_REG0}}, // add r0 r1 r0
+    {CARP_INSTR_DECR, {CARP_REG1}}, // decr r1
+    {CARP_INSTR_CMP, {CARP_REG1, CARP_REG2}}, // cmp r1 r2
+    {CARP_INSTR_RJZ, {2}}, // rjz 2 (jump forward +2 if eax is 0)
+                           // that is, to the halt instruction
+    {CARP_INSTR_JMP, {3}}, // jmp to more addition
+    {CARP_INSTR_SHOW, {CARP_REG0}}, // show r0
+    {CARP_INSTR_HALT, {0}}, // halt 0
   };
 
   carp_vm_do(program);
