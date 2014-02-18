811b44d004283143e448f028620d7623c7362b49
Max Bernstein <tekknolagi@gmail.com>
2014-02-18 00:39:28 -0700

    function name fix

--- carp.c
+++ carp.c
@@ -2,7 +2,7 @@
 
 int main (int argc, char** argv) {
   carp_machine_state mstate;
-  carp_init_vm(&mstate);
+  carp_vm_init(&mstate);
 
   carp_instruction program[] = {}; // something
   carp_vm_do(&mstate, program);
