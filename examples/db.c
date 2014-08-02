cae57357d19a767c88fee8f02ef73d06a8a155f9
Max Bernstein <tekknolagi@gmail.com>
2014-08-02 12:53:47 +0200

    db example

---
+++ examples/db.c
@@ -0,0 +1,21 @@
+#include <carp/carp.h>
+
+int main (int argc, char **argv) {
+  char data[][CARP_HT_KEY_LENGTH] = {
+    "max"
+  };
+
+  long long strp = (long long) &data[0];
+
+  long long code[] = {
+    CARP_INSTR_DBS, strp, 10,
+    CARP_INSTR_DBG, strp,
+    CARP_INSTR_PREG, CARP_EAX,
+    CARP_INSTR_HALT, 0
+  };
+
+  carp_machine_state m;
+  carp_vm_init(&m, 1, 0);
+  carp_vm_load(&m, code);
+  carp_vm_run(&m);
+}
