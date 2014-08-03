fd3c2439a1badb59f28e0f6d8ba1b2890e9cbeca
Max Bernstein <tekknolagi@gmail.com>
2014-08-03 13:32:18 -0700

    moar options

--- src/carp.c
+++ src/carp.c
@@ -1,7 +1,71 @@
+#include <getopt.h>
+
 #include "carp.h"
 #include "carp_lexer.h"
 
 int main (int argc, char **argv) {
+  static struct option long_options[] = {
+    {"version", 0, 0, 'v'},
+    {"license", 0, 0, 'l'},
+  };
+
+  // flags
+  carp_option opts = {0, 0, 0, 0};
+  // endflags
+
+  int c;
+  int option_index = 0;
+  while ((c = getopt_long(argc, argv, "vl", long_options, &option_index)) != -1) {
+    int this_option_optind = optind ? optind : 1;
+    switch (c) {
+    case 0:
+      printf("option %s", long_options[option_index].name);
+      if (optarg)
+	printf(" with arg %s", optarg);
+      puts("");
+      break;
+
+    case 'v':
+      opts.version = 1;
+      break;
+
+    case 'l':
+      opts.license = 1;
+      break;
+
+    case 'w':
+      opts.warranty = 1;
+      break;
+
+    case 'c':
+      opts.conditions = 1;
+      break;
+
+    default:
+      printf("?? getopt returned character code 0%o ??\n", c);
+    }
+
+    if (optind < argc) {
+      printf("non-option argv elements: ");
+      while (optind < argc)
+	printf("%s ", argv[optind++]);
+      puts("");
+    }
+  }
+
+  if (options.version)
+    carp_print_version();
+
+  if (options.license)
+    carp_print_license();
+
+  if (options.warranty)
+    carp_print_warranty();
+
+  if (options.conditions)
+    carp_print_conditions();
+
+  /*
   char *fn = argv[1];
   if (fn == NULL) {
     fprintf(stderr, "No filename given.\n");
@@ -17,7 +81,26 @@ int main (int argc, char **argv) {
   carp_machine_state m;
   carp_lex_lex(&m, tokens);
   carp_vm_run(&m);
-  carp_vm_cleanup(&m);
+  carp_vm_cleanup(&m);*/
 
   return 0;
+}
+
+void carp_print_version () {
+  printf("carp %1.1f\n", CARP_VERSION);
+}
+
+void carp_print_license () {
+  puts("carp  Copyright (C) 2014  Maxwell Bernstein\
+    This program comes with ABSOLUTELY NO WARRANTY; for details type `carp -w'.\
+    This is free software, and you are welcome to redistribute it\
+    under certain conditions; type `carp -c' for details.");
+}
+
+void carp_print_warranty () {
+  puts("THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.");
+}
+
+void carp_print_conditions () {
+  puts("See LICENSE.txt or http://www.gnu.org/licenses/gpl-3.0.txt");
 }
--- src/carp.h
+++ src/carp.h
@@ -3,4 +3,18 @@
 
 #include "carp_machine.h"
 
+#define CARP_VERSION 0.1
+
+typedef struct carp_option_s {
+  short int version;
+  short int license;
+  short int warranty;
+  short int conditions;
+} carp_option;
+
+void carp_print_version ();
+void carp_print_license ();
+void carp_print_warranty ();
+void carp_print_conditions ();
+
 #endif
