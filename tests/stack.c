c14b98bc9b4e33a804ab0096585df312ddea9707
Max Bernstein <tekknolagi@gmail.com>
2014-08-12 13:18:29 -0700

    put color responsibility on libtap

--- tests/stack.c
+++ tests/stack.c
@@ -1,22 +1,36 @@
 #include "tests.h"
+#include "../src/lib/carp_stack.h"
 
 int main (int argc, char **argv) {
   int status;
   carp_stack s;
   long long esp;
+  long long val;
+  long long to_push = 1;
 
   plan(NO_PLAN);
 
   status = carp_stack_init(&s, &esp, 10);
-  ok(status == 0, AC_OK("Made stack."));
+  ok(status == 0, "Made stack.");
 
   status = carp_stack_empty(&s);
-  ok(status == 1, AC_OK("Stack is empty by default."));
+  ok(status == 1, "Stack is empty by default.");
+
+  status = carp_stack_pop(&s, &val);
+  ok(status == -1, "Error popping empty stack.");
 
   status = carp_stack_full(&s);
-  ok(status == 0, AC_OK("Stack is not full by default."));
+  ok(status == 0, "Stack is not full by default.");
+
+  status = carp_stack_push(&s, to_push);
+  ok(status == 0, "Pushed successfully.");
+
+  status = carp_stack_pop(&s, &val);
+  ok(status == 0, "Popped value successfully.");
+  ok(val == to_push, "Popped same value as pushed.");
 
   carp_stack_cleanup(&s);
+  ok(s.contents == NULL, "Contents are NULL.");
 
   done_testing();
 }
--- tests/tests.h
+++ tests/tests.h
@@ -1,19 +1,6 @@
 #ifndef TESTS_H
 #define TESTS_H
 
-#include "../src/carp.h"
 #include "libtap/tap.h"
-
-#define AC_RED     "\x1b[31m"
-#define AC_GREEN   "\x1b[32m"
-#define AC_YELLOW  "\x1b[33m"
-#define AC_BLUE    "\x1b[34m"
-#define AC_MAGENTA "\x1b[35m"
-#define AC_CYAN    "\x1b[36m"
-#define AC_RESET   "\x1b[0m"
-
-#define AC_CLR(color, text) (AC_##color text AC_RESET)
-#define AC_OK(text) AC_CLR(GREEN, text)
-#define AC_BAD(text) AC_CLR(RED, text)
 
 #endif
