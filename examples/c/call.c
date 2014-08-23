798223adc106c834f6271fb963a4f3a2997c0f50
Max Bernstein <tekknolagi@gmail.com>
2014-08-23 01:11:33 -0700

    update c api example

--- examples/c/call.c
+++ examples/c/call.c
@@ -2,16 +2,17 @@
 
 int main (int argc, char **argv) {
   carp_value code[] = {
-    CARP_INSTR_LOAD, -5, // 1st arg
-    CARP_INSTR_LOAD, -4, // 2nd arg
+    CARP_INSTR_LOAD, -4, // 1st arg
+    CARP_INSTR_LOAD, -3, // 2nd arg
     CARP_INSTR_ADD,
+    CARP_INSTR_POP, CARP_REG_AX,
     CARP_INSTR_RET,
 
     CARP_INSTR_PUSH, 7, // 1st arg
     CARP_INSTR_PUSH, 9, // 2nd arg
-    CARP_INSTR_CALL, 0, 2,
+    CARP_INSTR_CALL, 0,
 
-    CARP_INSTR_PTOP,
+    CARP_INSTR_PREG, CARP_REG_AX,
 
     CARP_INSTR_HALT, 0
   };
