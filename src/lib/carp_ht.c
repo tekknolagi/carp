127b47f70a5693741dd354dd387a6db7d58f6f18
Max Bernstein <tekknolagi@gmail.com>
2014-09-06 21:23:27 -0400

    somewhat cleaner but pointer errors :O

--- src/lib/carp_ht.c
+++ src/lib/carp_ht.c
@@ -1,106 +1,32 @@
 #include "carp_ht.h"
 
-static unsigned long carp_ht_rhash (const char *, long);
+static unsigned long carp_ht_rhash (const char *);
 static unsigned long carp_ht_hash (const char *, long);
 static short int carp_ht_used (carp_ht *);
 
 int main () {
   carp_ht h;
   int status;
-  printf("jesus\n");
-  status = carp_ht_init(&h, 10);
-  if (status == 1) {
-    fprintf(stderr, "Memory could not be allocated.\n");
-    exit(1);
-  }
+
+  carp_ht_init(&h, 10);
+
+  carp_ht_set(&h, "Maxwell\0", 17);
+  carp_ht_set(&h, "llewxaM\0", 18);
+  carp_ht_set(&h, "axwellM\0", 19);
+  carp_ht_set(&h, "a\0", 19);
+  carp_ht_set(&h, "b\0", 19);
+  carp_ht_set(&h, "c\0", 19);
+  carp_ht_set(&h, "d\0", 19);
+  carp_ht_set(&h, "e\0", 19);
+  carp_ht_set(&h, "f\0", 19);
+  carp_ht_set(&h, "g\0", 19);
+  carp_ht_set(&h, "h\0", 19);
+
   carp_ht_print(&h);
 
-  status = carp_ht_set(&h, "Maxwell\0", 17);
-  if (status == 1) {
-    fprintf(stderr, "Collision! Gotta figure out how to handle this.\n");
-    carp_ht_cleanup(&h);
-    exit(1);
-  }
-  carp_ht_print(&h);
+  status = carp_ht_del(&h, "Maxwell\0");
+  printf("del status: %d\n", status);
 
-  status = carp_ht_set(&h, "llewxaM\0", 18);
-  if (status == 1) {
-    fprintf(stderr, "llewxaM == Maxwell\n");
-    carp_ht_cleanup(&h);
-    exit(1);
-  }
-  carp_ht_print(&h);
-
-  status = carp_ht_set(&h, "axwellM\0", 19);
-  if (status == 1) {
-    fprintf(stderr, "llewxaM == Maxwell\n");
-    carp_ht_cleanup(&h);
-    exit(1);
-  }
-  carp_ht_print(&h);
-
-  status = carp_ht_set(&h, "xwellMa\0", 20);
-  if (status == 1) {
-    fprintf(stderr, "llewxaM == Maxwell\n");
-    carp_ht_cleanup(&h);
-    exit(1);
-  }
-  carp_ht_print(&h);
-
-  status = carp_ht_set(&h, "wellMax\0", 21);
-  if (status == 1) {
-    fprintf(stderr, "llewxaM == Maxwell\n");
-    carp_ht_cleanup(&h);
-    exit(1);
-  }
-  carp_ht_print(&h);
-
-  status = carp_ht_set(&h, "ellMaxw\0", 22);
-  if (status == 1) {
-    fprintf(stderr, "llewxaM == Maxwell\n");
-    carp_ht_cleanup(&h);
-    exit(1);
-  }
-  carp_ht_print(&h);
-
-  status = carp_ht_set(&h, "llMaxwe\0", 23);
-  if (status == 1) {
-    fprintf(stderr, "llewxaM == Maxwell\n");
-    carp_ht_cleanup(&h);
-    exit(1);
-  }
-  carp_ht_print(&h);
-
-  status = carp_ht_set(&h, "lMaxwel\0", 24);
-  if (status == 1) {
-    fprintf(stderr, "llewxaM == Maxwell\n");
-    carp_ht_cleanup(&h);
-    exit(1);
-  }
-  carp_ht_print(&h);
-
-  status = carp_ht_set(&h, "!\"axwell\0", 25);
-  if (status == 1) {
-    fprintf(stderr, "llewxaM == Maxwell\n");
-    carp_ht_cleanup(&h);
-    exit(1);
-  }
-  carp_ht_print(&h);
-
-  status = carp_ht_set(&h, "%(xwell\0", 25);
-  if (status == 1) {
-    fprintf(stderr, "llewxaM == Maxwell\n");
-    carp_ht_cleanup(&h);
-    exit(1);
-  }
-  carp_ht_print(&h);
-
-  status = carp_ht_set(&h, "(%xwell\0", 25);
-  if (status == 1) {
-    fprintf(stderr, "llewxaM == Maxwell\n");
-    carp_ht_cleanup(&h);
-    exit(1);
-  }
   carp_ht_print(&h);
 
   carp_ht_cleanup(&h);
@@ -159,68 +85,76 @@ short int carp_ht_del (carp_ht *h, const char *key) {
   assert(key != NULL);
 
   unsigned long hash = carp_ht_hash(key, h->size);
-  if (h->buckets[hash] == NULL) return 0;
+  carp_ht_entry *base = h->buckets[hash];
+  carp_ht_entry *prev = NULL;
 
-  // different key, same hash! collision!
-  if (!strcmp(h->buckets[hash]->key, key)) {
-    carp_ht_entry *base = h->buckets[hash];
-    while (!strcmp(base->next->key, key)) { //possible issue if key is empty string?
-      if (base->next == NULL) return 0;
+  // nothing in hashed bucket; error
+  if (base == NULL) return 1;
+
+  // first bucket is right bucket
+  if (!strcmp(base->key, key)) {
+    h->buckets[hash] = base->next;
+    free(base);
+  }
+
+  else {
+    // look for bucket in chain
+    while (strcmp(base->key, key) != 0) {
+      // not found
+      if (base->next == NULL) return 2;
+
+      prev = base;
       base = base->next;
     }
-    carp_ht_entry *next = base->next;
-    base->next = base->next->next;
-    free(next);
-  }
-  // unused bucket
-  if (!h->buckets[hash])
-    return 2;
 
-  carp_ht_entry *next = h->buckets[hash]->next;
-  h->buckets[hash]->next = h->buckets[hash]->next->next;
-  free(next);
+    // if base is last, then next is NULL
+    // if base is not last, then next is next in chain
+    prev->next = base->next;
+    free(base);
+  }
 
   return 0;
 }
+
 
 short int carp_ht_set (carp_ht *h, const char *key, long long value) {
   assert(h != NULL);
   assert(key != NULL);
 
-  carp_ht_entry *new = calloc(1, sizeof(carp_ht_entry));
-
-  unsigned long rhash = carp_ht_rhash(key, h->size);
-  unsigned long hash = rhash % h->size;
-
-  if (h->buckets[hash] == NULL) {
-    strncpy(new->key, key, strlen(key));
-    new->value = value;
-    h->buckets[hash] = new;
-    return 0;
+  // too full? resize
+  if (carp_ht_used(h) > 60) {
+    if (carp_ht_resize(h) == 1)
+      return 1;
+    else if (carp_ht_set(h, key, value) != 0)
+      return 1;
   }
 
-  // different key, same hash! collision!
-  // TODO: linear probing!
-  carp_ht_entry *base = h->buckets[hash];
-  while (base->next != NULL && strcmp(base->next->key, key) != 0) {
-    // resize? o.O
-    if (hash >= h->size || carp_ht_used(h) > 60) {
-      if (carp_ht_resize(h) == 1) {
-	fprintf(stderr, "Could not resize.\n");
-	return 1;
-      }
-      else {
-	carp_ht_set(h, key, value);
+  carp_ht_entry *base = carp_ht_get(h, key);
+
+  // unused bucket
+  if (base == NULL) {
+    unsigned long hash = carp_ht_hash(key, h->size);
+    h->buckets[hash] = calloc(1, sizeof *base);
+    base = h->buckets[hash];
+  }
+  // in the chain?
+  else {
+    while (base->next) {
+      if (strcmp(base->next->key, key) != 0) {
+	base->next->value = value;
 	return 0;
       }
+
+      base = base->next;
     }
 
+    base->next = calloc(1, sizeof base->next);
     base = base->next;
   }
 
-  base->next = new;
-  strncpy(new->key, key, strlen(key));
-  new->value = value;
+  strncpy(base->key, key, strlen(key));
+  base->value = value;
+  base->next = NULL;
 
   return 0;
 }
@@ -229,10 +163,9 @@ carp_ht_entry *carp_ht_get (carp_ht *h, const char *key) {
   assert(h != NULL);
   assert(key != NULL);
 
-  unsigned long rhash = carp_ht_rhash(key, h->size);
-  unsigned long hash = rhash % h->size;
-
+  unsigned long hash = carp_ht_hash(key, h->size);
   carp_ht_entry *base = h->buckets[hash];
+
   while (base && strcmp(base->key, key) != 0) {
     if (hash >= h->size) return NULL; // not found
 
@@ -243,31 +176,30 @@ carp_ht_entry *carp_ht_get (carp_ht *h, const char *key) {
 }
 
 short int carp_ht_resize (carp_ht *h) {
-  //TODO: This probably still leaks memory, did not try to free entry lists...
+  // TODO: This probably still leaks memory, did not try to free entry lists...
   assert(h != NULL);
 
   long newsize = 2 * h->size + 1;
-  carp_ht newh = {newsize, NULL};
-  newh.buckets = calloc(newsize, sizeof(carp_ht_entry *));
+  carp_ht newh = { newsize, NULL };
+
+  newh.buckets = calloc(newsize, sizeof(*newh.buckets));
   if (newh.buckets == NULL)
     return 1;
 
   for (long i = 0; i < h->size; i++)
     if (h->buckets[i]) {
       carp_ht_entry *base = h->buckets[i];
+
       while (base) {
 	const char *key = base->key;
 	unsigned long hash = carp_ht_hash(key, h->size);
-	printf("adding [%ld] %s, ", hash, key);
+
 	carp_ht_set(&newh, base->key, base->value);
-	carp_ht_entry *next = base->next;
-	free(base);
-	base = next;
+	base = base->next;
       }
-      printf("\n");
     }
 
-  free(h->buckets);
+  carp_ht_cleanup(h);
   h->size = newsize;
   h->buckets = newh.buckets;
   return 0;
@@ -283,7 +215,7 @@ void carp_ht_print (carp_ht *h) {
     if (h->buckets[i]) {
       carp_ht_entry *base = h->buckets[i];
       do {
-	printf("  \"%s\": %lld,", base->key, base->value);
+	printf("  [%ld] \"%s\": %lld,", i, base->key, base->value);
       } while ((base = base->next));
       printf("\n");
     }
@@ -292,17 +224,19 @@ void carp_ht_print (carp_ht *h) {
 }
 
 void carp_ht_cleanup (carp_ht *h) {
-
-  //TODO: Definitely leaks memory.
+  // TODO: Definitely leaks memory.
   assert(h != NULL);
+
   for (long i = 0; i < h->size; i++)
     if (h->buckets[i]) {
       carp_ht_entry *base = h->buckets[i];
-      while (base) {
+
+       while (base) {
 	carp_ht_entry *next = base->next;
 	free(base);
         base = next;
       }
     }
+
   free(h->buckets);
 }
